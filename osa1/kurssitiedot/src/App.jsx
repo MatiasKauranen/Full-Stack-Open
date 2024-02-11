cc504512fabc446135f18b461fbdf0e134b905a0
MatiasKauranen <94970023+MatiasKauranen@users.noreply.github.com>
2024-02-12 00:16:07 +0200

    1.4
    Oliot on nyt taulukossa.

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -16,10 +16,10 @@ const Content = ({ parts }) => {
   return <div>{partComponents}</div>;
 };
 
-const Part = (content) => {
+const Part = ({ part, exercise }) => {
   return (
     <p>
-      {content.part}: {content.exercise}
+      {part}: {exercise}
     </p>
   );
 };
@@ -31,20 +31,20 @@ const Total = ({ parts }) => {
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  };
-
-  const parts = [part1, part2, part3]; // Array containing all parts
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ];
 
   return (
     <div>
