a305e91448cc6f172bfeb916e752d1f2ef082b44
MatiasKauranen <94970023+MatiasKauranen@users.noreply.github.com>
2024-02-16 00:35:39 +0200

    1.7

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -5,6 +5,8 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const total = good + neutral + bad;
+  const average = total > 0 ? (good - bad) / total : 0;
 
   return (
     <div>
@@ -26,6 +28,9 @@ const App = () => {
       good {good}<br />
       neutral {neutral}<br />
       bad {bad}<br />
+      all {total}<br />
+      average {average}<br />
+      positive {(good / total) * 100} % <br />
     </div>
   )
 }
