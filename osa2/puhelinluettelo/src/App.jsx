91f9a7e23f37f5366f98949a2b70b56f0d372df3
MatiasKauranen <94970023+MatiasKauranen@users.noreply.github.com>
2024-02-29 21:24:36 +0200

    2.11: puhelinluettelo step6

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,13 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './components/SearchBar';
 import AddPerson from './components/AddPerson';
 import PersonList from './components/PersonList';
+import axios from 'axios';
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchQuery, setSearchQuery] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching initial state:', error);
+      });
+  }, []);
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -28,9 +39,15 @@ const App = () => {
       return;
     }
     const newPerson = { name: newName, number: newNumber };
-    setPersons([...persons, newPerson]);
-    setNewName('');
-    setNewNumber('');
+    axios.post('http://localhost:3001/persons', newPerson)
+      .then(response => {
+        setPersons([...persons, response.data]);
+        setNewName('');
+        setNewNumber('');
+      })
+      .catch(error => {
+        console.error('Error adding new person:', error);
+      });
   };
 
   const filteredPersons = persons.filter(person =>
